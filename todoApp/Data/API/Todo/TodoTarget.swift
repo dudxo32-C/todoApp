99d22681e13cbdafafbcdb37d96b34fc2d0c7e53
dudxo32 <dudxo32@gmail.com>
2025-05-08 15:31:21 +0900

    [TODO-19] Moya Stub 데이터 저장

--- todoApp/Data/API/Todo/TodoTarget.swift
+++ todoApp/Data/API/Todo/TodoTarget.swift
@@ -8,8 +8,24 @@
 import Foundation
 import Moya
 
+final class SampleDataLoader {
+    static func loadJSON(named fileName: String) -> Data {
+        guard
+            let url = Bundle.main.url(
+                forResource: fileName, withExtension: "json")
+        else {
+            fatalError("Missing file: \(fileName).json")
+        }
+        return try! Data(contentsOf: url)
+    }
+}
+
 enum TodoAPI {
     case fetchList
+    case write(todo: TodoModelProtocol)
+    case delete(id: String)
+    case update(todo: TodoModelProtocol)
+
 }
 
 extension TodoAPI: TargetType {
@@ -17,11 +33,45 @@ extension TodoAPI: TargetType {
         switch self {
         case .fetchList:
             return .get
+        case .write(let todo):
+            return .post
+        case .delete(let id):
+            return .delete
+        case .update(let todo):
+            return .put
         }
     }
 
     var task: Moya.Task {
-        return .requestPlain
+        switch self {
+        case .fetchList:
+            return .requestPlain
+        case .write(let todo):
+            return .requestParameters(
+                parameters: [
+                    "id": todo.id,
+                    "title": todo.title,
+                    "completed": todo.isDone,
+                    "date": todo.date,
+                    "contents": todo.contents,
+                ], encoding: URLEncoding.httpBody
+            )
+        case .delete(let id):
+            return .requestParameters(
+                parameters: ["id": id],
+                encoding: URLEncoding.queryString
+            )
+        case .update(let todo):
+            return .requestParameters(
+                parameters: [
+                    "id": todo.id,
+                    "title": todo.title,
+                    "completed": todo.isDone,
+                    "date": todo.date,
+                    "contents": todo.contents,
+                ], encoding: URLEncoding.queryString
+            )
+        }
     }
 
     var headers: [String: String]? {
@@ -36,6 +86,25 @@ extension TodoAPI: TargetType {
         switch self {
         case .fetchList:
             return "todos"
+        case .write(let todo):
+            return "write"
+        case .delete(let id):
+            return "delete"
+        case .update(let todo):
+            return "update"
+        }
+    }
+
+    var sampleData: Data {
+        switch self {
+        case .fetchList:
+            return SampleDataLoader.loadJSON(named: "fetch")
+        case .write(let todo):
+            return try! JSONSerialization.data(withJSONObject: todo)
+        case .delete(let id):
+            return try! JSONSerialization.data(withJSONObject: ["id": id])
+        case .update(let todo):
+            return try! JSONSerialization.data(withJSONObject: todo)
         }
     }
 }
